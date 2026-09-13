22972b6f69560d4e33877e17e04fbb01f26bebf2
fabiocanciani <95185753+fabiocanciani@users.noreply.github.com>
2026-09-13 20:23:15 +0200

    Align Java and Kotlin JVM targets to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,10 @@ android {
         versionName = "1.0"
     }
     buildFeatures { viewBinding = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions { jvmTarget = "17" }
 }
 dependencies {
